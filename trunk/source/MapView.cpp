7dda8083ccffb392b7743899f95a2e4f9452f2cf
Mikael Lindlöf <mikuz.dev@gmail.com>
2013-10-20 13:56:14 +0000

    Show fight button in map mode.

--- trunk/source/MapView.cpp
+++ trunk/source/MapView.cpp
@@ -14,21 +14,23 @@
 #include "IwGraphics.h"
 
 void renderMap();
+void renderFight();
+
+static CIwTexture* fightTexture;
 
 static CIwTexture* ghostTexture;
 static CIwTexture* playerTexture;
 static CIwTexture* mapTexture;
 
 void mapInit(int mapW, int mapH);
-static CIwSVec2 mapVert[4];
-static CIwFVec2 mapUvs[4];
+static CIwFVec2 mapVert[4];
 
-static CIwFVec2 default_uvs[4] =
+static CIwFVec2 mapDefaultUvs[4] =
 {
     CIwFVec2(0, 0),
     CIwFVec2(0, 1),
     CIwFVec2(1, 1),
-    CIwFVec2(1, 0),
+    CIwFVec2(1, 0)
 };
 
 void MapViewInit()
@@ -36,6 +38,10 @@ void MapViewInit()
 	// Set screen clear colour
     IwGxSetColClear(0xff, 0xff, 0xff, 0xff);
     IwGxPrintSetColour(128, 128, 128);
+
+	fightTexture = new CIwTexture;
+	fightTexture->LoadFromFile ("textures/map_fight.png");
+	fightTexture->Upload();
 	
 	ghostTexture = new CIwTexture;
 	ghostTexture->LoadFromFile ("textures/map_ghost.png");
@@ -45,6 +51,10 @@ void MapViewInit()
 	playerTexture->LoadFromFile ("textures/map_player.png");
 	playerTexture->Upload();
 	
+	/*default_uvs[0] = CIwFVec2(0, 0);
+    default_uvs[1] = CIwFVec2(0, 1);
+    default_uvs[2] = CIwFVec2(1, 1);
+    default_uvs[3] = CIwFVec2(1, 0);*/
 	
 	mapTexture = new CIwTexture;
 	mapTexture->LoadFromFile ("textures/map_template.png");
@@ -80,6 +90,9 @@ void mapInit(int mapW, int mapH) {
 
 void MapViewTerm() {
 
+	if (fightTexture)
+		delete fightTexture;
+
 	if (ghostTexture)
 		delete ghostTexture;
 
@@ -96,6 +109,8 @@ bool MapViewUpdate() {
     IwGxClear(IW_GX_COLOUR_BUFFER_F | IW_GX_DEPTH_BUFFER_F);
 
 	renderMap();
+
+	renderFight();
 
 	IwGxFlush();
     IwGxSwapBuffers();
@@ -114,8 +129,33 @@ void renderMap() {
 
 	IwGxSetScreenSpaceSlot(-1);
 
-    IwGxSetUVStream(default_uvs);
+    IwGxSetUVStream(mapDefaultUvs);
     IwGxSetVertStreamScreenSpace(mapVert, 4);
 
     IwGxDrawPrims(IW_GX_QUAD_LIST, NULL, 4);
+}
+
+void renderFight() {
+	IwGxLightingOff();
+	
+	CIwMaterial* pMat = IW_GX_ALLOC_MATERIAL();
+    pMat->SetModulateMode(CIwMaterial::MODULATE_NONE);
+	pMat->SetAlphaMode(CIwMaterial::ALPHA_BLEND);
+    pMat->SetTexture(fightTexture);
+
+	int16 w = IwGxGetScreenWidth() * 0.7f;
+	if (w > (int16)fightTexture->GetWidth()) w = (int16)fightTexture->GetWidth();
+
+	float whScale = (float)((double)fightTexture->GetWidth() / fightTexture->GetHeight());
+	int16 h = w * 1/whScale;
+	int bottomPadding = IwGxGetScreenHeight() * 0.03f;
+
+	int16 x1 = IwGxGetScreenWidth()/2 - w/2;
+    int16 y1 = IwGxGetScreenHeight() - h - bottomPadding;
+
+	IwGxSetMaterial(pMat);
+    IwGxSetUVStream(mapDefaultUvs);
+
+    CIwSVec2 XY(x1, y1), dXY(w, h);
+	IwGxDrawRectScreenSpace(&XY, &dXY);
 }
